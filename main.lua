491b8ac743e03745ad5f36c7dbbba90fce497c15
TKFPc30 <aisyrapid@gmail.com>
2025-07-02 17:38:24 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,28 +1,25 @@
 local player = game.Players.LocalPlayer
-	local char = player.Character or player.CharacterAdded:Wait()
+local char = player.Character or player.CharacterAdded:Wait()
 
-	task.spawn(function()
-		local meshTypes = {"Brick", "Cylinder", "Head", "Sphere", "Torso", "Wedge"}
+task.spawn(function()
+	local meshTypes = {"Brick", "Cylinder", "Head", "Sphere", "Torso", "Wedge"}
 
-		for _, partName in pairs({"Head", "Torso", "Left Arm", "Right Arm", "Left Leg", "Right Leg"}) do    
-			local part = char:FindFirstChild(partName)    
-			if part then    
-				local mesh = part:FindFirstChildOfClass("SpecialMesh") or Instance.new("SpecialMesh", part)    
-				mesh.MeshType = Enum.MeshType.Head    
+	for _, partName in pairs({"Head", "Torso", "Left Arm", "Right Arm", "Left Leg", "Right Leg"}) do    
+		local part = char:FindFirstChild(partName)    
+		if part then    
+			local mesh = part:FindFirstChildOfClass("SpecialMesh") or Instance.new("SpecialMesh", part)    
+			mesh.MeshType = Enum.MeshType.Head    
+		end    
+	end    
+
+	while true do    
+		wait(0.1)    
+		for _, part in ipairs(char:GetChildren()) do    
+			if part:IsA("Part") and part:FindFirstChildOfClass("SpecialMesh") then    
+				local mesh = part:FindFirstChildOfClass("SpecialMesh")    
+				mesh.MeshType = Enum.MeshType[meshTypes[math.random(1, #meshTypes)]]    
+				part.BrickColor = BrickColor.Random()    
 			end    
 		end    
-
-		while true do    
-			wait(0.1)    
-			for _, part in ipairs(char:GetChildren()) do    
-				if part:IsA("Part") and part:FindFirstChildOfClass("SpecialMesh") then    
-					local mesh = part:FindFirstChildOfClass("SpecialMesh")    
-					mesh.MeshType = Enum.MeshType[meshTypes[math.random(1, #meshTypes)]]    
-					part.BrickColor = BrickColor.Random()    
-				end    
-			end    
-		end
-
-	end)
-
+	end
 end)
